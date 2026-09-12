c6132ffb79385dfcd850b2108f38d2abdd4a4ac4
Waeljakson <waeljaksonstar@gmail.com>
2026-09-12 19:43:44 +0300

    Bundle Cairo font and premium theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/cairo/400.css";
+import "@fontsource/cairo/500.css";
+import "@fontsource/cairo/600.css";
+import "@fontsource/cairo/700.css";
+import "@fontsource/cairo/800.css";
 import "./styles.css";
+import "./premium.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
